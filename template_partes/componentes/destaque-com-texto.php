f5d589c599d2ccffacb94a6beda688aa280e7c8f
Rodrigo Godoy <contato@rodrigogodoy.com.br>
2018-05-25 10:14:16 -0300

    comentado o botão orçamento no meio da página

--- template_partes/componentes/destaque-com-texto.php
+++ template_partes/componentes/destaque-com-texto.php
@@ -6,9 +6,10 @@
                     <p class="destaque-com-texto__texto">As imagens com drone proporcionam qualidade para apresentar seu projeto, seja ele publicitário, curta ou o dlipe da sua banda.</p>
                     <p class="destaque-com-texto__texto">Também podem ser usadas imagens aéreas com drone para acompanhamento de obras, verificação de telhados, auxilio para reconhecimento de terrenos, entre outras infinitas possibilidades.</p>
                 </div>
+                <!--
                 <div class="col-md-5 col-sm-12 destaque-com-texto__botao">
                     <a href="#orcamento" type="button" class="btn btn-primary btn-lg">Orçamento</a>
-                </div>
+                </div>-->
             </div>
         </div>
         <div class="destaque-com-texto__overlay"></div>
