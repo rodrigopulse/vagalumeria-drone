0492dafa952404819c49bbf496e937fd2ee6d778
Rodrigo Godoy <contato@rodrigogodoy.com.br>
2018-05-25 08:01:04 -0300

    trocado o texto do footer

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
         <div class="container footer-contato__formulario">
             <div class="container">
                 <h1 class="footer-contato__titulo">Orçamento</h1>
-                <p class="lead">Faça um orçamento com a gente e se surpreenda com o quanto é barato fazer imagens aéreas com drones :)</p>
+                <p class="lead">Faça um orçamento com a gente e se surpreenda com ótimos preços para fazer imagens aéreas com drones :)</p>
             </div>
             <?php echo do_shortcode( '[contact-form-7 id="9" title="Contato"]' ); ?>
         </div>
