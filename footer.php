2550ff509368b8686c4f9897767a6361f98fe85a
Rodrigo Godoy <contato@rodrigogodoy.com.br>
2018-05-25 09:39:01 -0300

    colocado o ninja forms

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@
                 <h1 class="footer-contato__titulo">Orçamento</h1>
                 <p class="lead">Faça um orçamento com a gente e se surpreenda com ótimos preços para fazer imagens aéreas com drones :)</p>
             </div>
-            <?php echo do_shortcode( '[contact-form-7 id="9" title="Contato"]' ); ?>
+            <?php echo do_shortcode( '[ninja_form id=2]' ); ?>
         </div>
     </div>
 </footer>
